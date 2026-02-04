7e274da409dd0c174e2a94eb253d33c56ecf353a
vitor <vitormwxm@gmail.com>
2026-02-03 21:05:06 -0300

    Client Service

--- src/main/java/com/clients/crud_api/services/ClientService.java
+++ src/main/java/com/clients/crud_api/services/ClientService.java
@@ -1,4 +1,23 @@
 package com.clients.crud_api.services;
 
+import com.clients.crud_api.dto.ClientDTO;
+import com.clients.crud_api.entities.Client;
+import com.clients.crud_api.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClientService {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    public List<ClientDTO> findAll() {
+        List<Client> client = clientRepository.findAll();
+        return client.stream().map(x -> new ClientDTO(x)).toList();
+    }
 }
